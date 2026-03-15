911119a53d6d09b45b7c0afd5f14c3cbab4e96bd
hydrogendeuteride <dumpsterowner@gmail.com>
2026-03-15 23:31:28 +0900

    FIX: reduce maneuver gizmo drag sensitivity

--- src/game/states/gameplay/maneuver/gameplay_state_maneuver_types.h
+++ src/game/states/gameplay/maneuver/gameplay_state_maneuver_types.h
@@ -109,7 +109,7 @@ namespace Game
         float icon_size_px{30.0f};
         float overlay_scale{1.0f};
         bool show_axis_labels{true};
-        float drag_sensitivity_mps_per_m{0.0025f};
+        float drag_sensitivity_mps_per_m{0.00025f};
     };
 
     struct ManeuverGizmoInteraction
